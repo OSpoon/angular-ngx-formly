6f8a08736c6667f06d980759657e2828203e5d9a
fe-xiaoxin <zxin088@gmail.com>
2024-01-06 22:49:55 +0800

    feat✨: 杂项

--- src/app/pages/verify/verify.component.ts
+++ src/app/pages/verify/verify.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { AbstractControl, FormGroup, ValidationErrors } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
-import { map, of, timer } from 'rxjs';
+import { map, timer } from 'rxjs';
 
 @Component({
   selector: 'app-verify',
@@ -10,53 +10,6 @@ import { map, of, timer } from 'rxjs';
   encapsulation: ViewEncapsulation.None,
 })
 export class VerifyComponent {
-  // form = new FormGroup({});
-  // model = { email: 'email@gmail.com' };
-  // fields: FormlyFieldConfig[] = [
-  //   {
-  //     key: 'ip',
-  //     type: 'ip',
-  //     props: {
-  //       label: 'IP Address',
-  //       required: true,
-  //     },
-  //     // validators: {
-  //     //   // validation: [IpValidator],
-  //     //   ip: {
-  //     //     expression: (control: AbstractControl) =>
-  //     //       /(\d{1,3}\.){3}\d{1,3}/.test(control.value),
-  //     //     message: (error: any, field: FormlyFieldConfig) =>
-  //     //       `${field.formControl?.value} 不是有效的 IP 地址`,
-  //     //   },
-  //     // },
-  //   },
-  //   {
-  //     key: 'telephone',
-  //     type: 'telephone',
-  //     props: {
-  //       label: 'Telephone',
-  //       required: true,
-  //     },
-  //     // asyncValidators: {
-  //     //   // validation: [TelephoneAsyncValidator],
-  //     //   telephone: {
-  //     //     expression: (control: AbstractControl) =>
-  //     //       timer(1000).pipe(
-  //     //         map(() =>
-  //     //           /^(\+?86)?1[3-9]\d{9}|\d{3}-?\d{8}$/.test(control.value)
-  //     //         )
-  //     //       ),
-  //     //     message: (error: any, field: FormlyFieldConfig) =>
-  //     //       `${field.formControl?.value} 不是有效的电话号码`,
-  //     //   },
-  //     // },
-  //   },
-  // ];
-
-  // onSubmit(model: any) {
-  //   console.log(model);
-  // }
-
   existingPhones = ['15321776071', '15321776072', '15321776073'];
 
   signInForm = new FormGroup({});
